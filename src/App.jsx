ec358ff10b4d0001b3909f37a0caf2deafdef8c5
sehrishbibi01-CS <sehrish.biibi01@gmail.com>
2026-08-10 22:11:44 +0500

    controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -299,3 +299,27 @@ function App() {
 }
 
 export default App;
+
+//  Controlled Components
+
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <h1>Controlled Component</h1>
+
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <h2>Your Name: {name}</h2>
+    </div>
+  );
+}
+
+export default App;
