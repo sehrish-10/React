5f217482a2f01b93d449f11b4be9ec8db9559a03
sehrishbibi01-CS <sehrish.biibi01@gmail.com>
2026-08-12 16:33:04 +0500

    practice code

--- src/App.jsx
+++ src/App.jsx
@@ -134,7 +134,7 @@ function App() {
 export default App; 
  */
 
-
+/* 
 import {useState} from 'react'
 import Student from './Student';
 
@@ -154,7 +154,27 @@ function App() {
     </div>
   );
 }
- export default App; 
+ export default App;  */
 
 
- 
+ import { useState } from 'react';
+ function App() {
+  const [count, setCount] = useState(0);
+return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1) }>Counter</button>
+    {
+      count==0?<h1>Condition 0</h1>
+      :count==1?<h1>Condition 1</h1>
+      :count==2?<h1>Condition 2</h1>
+      :count==3?<h1>Condition 3</h1>
+      :count==4?<h1>Condition 4</h1>
+      :<h1>Other condition</h1> 
+    }
+    
+    </div>
+  );
+}
+export default App;
+
