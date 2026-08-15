bb5120748b4e7db05ac022afe8890608496eea0d
sehrishbibi01-CS <sehrish.biibi01@gmail.com>
2026-08-15 18:18:01 +0500

    use effect code

--- src/App.jsx
+++ src/App.jsx
@@ -573,7 +573,7 @@ function App() {
 export default App; */
 
 
-import { useState } from "react";
+/* import { useState } from "react";
 
 function InputBox({ name, setName }) {
   return (
@@ -609,6 +609,37 @@ function App() {
       <InputBox name={name} setName={setName} />
 
       <DisplayName name={name} />
+    </div>
+  );
+}
+
+export default App; */
+
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
